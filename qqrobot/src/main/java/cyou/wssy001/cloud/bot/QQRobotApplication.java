c765965bfa03de03c19ffe9cc403d217d7a8aee0
wssy001 <peter003@foxmail.com>
2021-12-25 21:42:06 +0800

    cleanup code

--- qqrobot/src/main/java/cyou/wssy001/cloud/bot/QQRobotApplication.java
+++ qqrobot/src/main/java/cyou/wssy001/cloud/bot/QQRobotApplication.java
@@ -1,6 +1,5 @@
 package cyou.wssy001.cloud.bot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableDiscoveryClient
 @SpringBootApplication
-@MapperScan("cyou.wssy001.cloud.bot.dao")
 public class QQRobotApplication {
 
     public static void main(String[] args) {
